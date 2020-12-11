045fad0af744709e379c6d694c9841b2021529ff
phatdroid94 <phatdroid94@gmail.com>
2020-12-11 20:28:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/viewmodel05102020/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.viewmodel05102020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
